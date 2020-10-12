863a1a50a9f170441cb4399ce52ba516411ac744
la-magbanua <la.magbanua18@gmail.com>
2020-10-12 14:38:05 +0800

    removed console.log statements from gatsby-node

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -101,11 +101,7 @@ async function turnSlicemastersIntoPages({ actions, graphql }) {
 
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
-  console.log(
-    `There are ${data.slicemasters.totalCount} total people. And we have ${pageCount} pages with ${pageSize} per page`
-  );
   Array.from({ length: pageCount }).forEach((_, i) => {
-    console.log(`Creating page ${i}`);
     actions.createPage({
       path: `/slicemasters/${i + 1}`,
       component: path.resolve('./src/pages/slicemasters.js'),
